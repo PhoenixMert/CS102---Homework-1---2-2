7866bb78bdcb564eefaab1a95d4f06102a555c2f
Musa Eren Üçdal <merenucdal25@gmail.com>
2025-02-05 12:57:42 +0300

    Update assignmentTwo.java
    I have fully completed the findAverage method.

--- assignmentTwo.java
+++ assignmentTwo.java
@@ -1,1 +1,11 @@
-
+public static void findAverage(int[] arr) {
+    int arrayTotal = 0;
+    int arrayAverage = 0;
+    for (int i = 0; i < arr.length; i++) {
+            arrayTotal += arr[i];
+    }
+    arrayAverage = arrayTotal / arr.length;
+    for (int i = 0; i < arr.length; i++) {
+        System.out.println(arr[i] - arrayAverage);
+    }
+}
